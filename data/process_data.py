5cc3eced88eff1785a24899294d63fbacbcc5696
dkudo <dkudo@ncsu.edu>
2019-01-07 00:05:01 -0500

    Modified train_classifier.py
    line43 : modified df["message", "genre"] to df["message"]
    line112 : modified "fit" to "predict"

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,89 @@
+# import packages
+import pandas as pd
+import numpy as np
+import sqlalchemy
+from sqlalchemy import create_engine
 import sys
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """Method to load 2 csv files and create 1 dataframe to be returned
+
+    Args:
+        message_filepath : path to the file containing messages
+        categories_filepath : path to the file containing categories where
+                                each of messages were classified
+
+    Returns:
+        df : merged dataframe containg messages and the labels
+    """
+
+    ### read in 2 files and merge them together
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = pd.merge(messages, categories, on="id")
+
+    return df
+
+
 
 
 def clean_data(df):
-    pass
+    """Method to clean the dataframe and return it
+
+    Args :
+        df : dataframe to be cleaned
+
+    Returns :
+        df : cleaned dataframe
+    """
+
+    # extract a dataframe of the 36 individual category columns
+    df_categories = df.categories.str.split(";", expand=True)
+
+    # make new column names for each of category columns
+    row = df_categories.iloc[0]
+    df_category_colnames = row.str[:-2].tolist()
+
+    # assign the new column names
+    df_categories.columns = df_category_colnames
+
+    # loop through each column of categories to extract 0 or 1 of each cell
+    for column in df_categories:
+
+        # set each value to be the last character of the strings
+        # then convert column from string to numeric
+        df_categories[column] = df_categories[column].str[-1].astype(int)
+
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = df.join(df_categories)
+
+    # drop the original 'categories' and 'child_alone' columns from `df`
+    df.drop(axis=1, labels=["categories", "child_alone"], inplace=True)
+
+    # drop duplicates
+    df = df.drop_duplicates()
+
+    # convert '2' in "related" column to '0'
+    df.related = df.related.replace(2, 0)
+
+    return df
 
 
-def save_data(df, database_filename):
-    pass  
+
+
+def save_data(df, database_filepath):
+    """Method to store the cleaned dataframe to a database file
+
+    Args:
+        df : dataframe which would be stored
+        database_filepath : path to the file where the data would be stored
+    """
+
+    engine = create_engine('sqlite:///{}'.format(database_filepath))
+    df.to_sql('messages', engine, index=False, if_exists="replace")
+
+
 
 
 def main():
@@ -24,12 +97,12 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
         print('Please provide the filepaths of the messages and categories '\
               'datasets as the first and second argument respectively, as '\
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,24 +1,131 @@
+# import libraries
+
 import sys
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sqlalchemy import create_engine
+import re
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk import WordNetLemmatizer
+
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import f1_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.metrics import classification_report
+import pickle
+
+nltk.download('wordnet')
 
 
 def load_data(database_filepath):
-    pass
+    """Method to load in data from the specified database_file
 
+    Args:
+        database_filepath : path to the database file to be loaded
+
+    Return :
+        X : matrix of messages and their genre
+        Y : matrix containing 36 labels of messages
+        category_names : series of 36 labels
+    """
+
+    # load data from database
+    engine = create_engine("sqlite:///{}".format(database_filepath))
+    df = pd.read_sql_table("messages", con=engine)
+
+    # split dataframe into x (messages and genres) / y (36 labels)
+    x = df["message"]
+    y = df.drop(axis=1, labels=["id", "message", "original", "genre"])
+
+    return x.values, y.values, y.columns
 
 def tokenize(text):
-    pass
+    """Method to make a list of tokens from sentences.
+
+    Args:
+        text : text to be tokenized
+
+    Return:
+        clean_tokens : list of clean tokens
+    """
+
+    # remove punctuations in the sentences
+    text = re.sub(r"[^a-zA-Z0-9]", " ", text)
+    # split sentences into tokens(words) in a list
+    tokens = word_tokenize(text)
+
+    # instantiate lemmatizer
+    lemmatizer = WordNetLemmatizer()
+    # create list where lemmatized tokens would be stored
+    clean_tokens = []
+
+    # lemmatize each token and store it in the list "clean_tokens"
+    for token in tokens:
+        clean_tok = lemmatizer.lemmatize(token).lower().strip()
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
 
 
 def build_model():
-    pass
+    """Method to return a GridSerach model object to be trained later.
+
+    Return:
+        cv : GridSearch model
+    """
+
+    pipeline = Pipeline([("vect",CountVectorizer(tokenizer=tokenize)),
+                     ("tfidf",TfidfTransformer()),
+                     ("clf", MultiOutputClassifier(RandomForestClassifier()))])
+
+    parameters = {
+    "vect__ngram_range" : [(1,1), (1,2)],
+    #"vect__max_df" : [0.5,1],
+    "vect__max_features" : [None, 5000, 10000],
+    #"tfidf__use_idf" : [True, False],
+    "clf__estimator__n_estimators" : [50,100,200],
+    "clf__estimator__min_samples_split" : [2,3,4]
+    }
+
+    cv = GridSearchCV(pipeline, param_grid=parameters, n_jobs=-1, cv=2)
+
+    return cv
+
 
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    """Evaluate and output the testing result of a model
+
+    Args:
+        model : a model used for testing
+        X_test : x matrix of testing data
+        Y_test : one-hot encoding y matrix of testing data
+        category_names : ordered category names
+    """
+
+    Y_pred = model.predict(X_test)
+
+    for i, category  in enumerate(category_names):
+        print(category)
+        print(classification_report(Y_test[:,i], Y_pred[:,i]))
 
 
 def save_model(model, model_filepath):
-    pass
+    """Method to save a model in a specified pickle file
+
+    Args:
+        model : a model to be saved
+        model_filepath : path to the file where a model would be stored
+    """
+
+    with open(model_filepath, 'wb') as file:
+        pickle.dump(model, file)
 
 
 def main():
@@ -27,13 +134,13 @@ def main():
         print('Loading data...\n    DATABASE: {}'.format(database_filepath))
         X, Y, category_names = load_data(database_filepath)
         X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
-        
+
         print('Building model...')
         model = build_model()
-        
+
         print('Training model...')
         model.fit(X_train, Y_train)
-        
+
         print('Evaluating model...')
         evaluate_model(model, X_test, Y_test, category_names)
 
